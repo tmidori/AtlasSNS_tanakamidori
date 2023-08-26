db25f074257fdeb152d68d139b84afa25c13ea16
田中翠 <>
2023-08-26 15:21:01 +0900

    投稿フォームを設置する

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。テスト</h2>
+<!-- <h2>機能を実装していきましょう。テスト</h2> -->
+<div class="container">
+  <img claas="user-icon" src="images/icon1.png">
+  {!! Form::open(['url' => 'post/create']) !!}
+  <div class="post-form">
+    {{Form::textarea('newPost', null, ['required','class' => 'post','placeholder' => '投稿内容を入力してください'])}}
+    <button type="submit" class=""><img src="images/post.png" alt="送信"></button>
+  </div>
+  {!! Form::close() !!}
+</div>
 
 @endsection
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.login')
 
+
 @section('content')
 
 
